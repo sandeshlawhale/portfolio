c0bba0a42a3b5268f42ba28426ffe46f26466e33
Sandesh <sandeshlawhale@gmail.com>
2026-01-17 00:12:14 +0530

    Feat: updated the CTA

---
+++ src/constants/quotes.ts
@@ -0,0 +1,54 @@
+const quotes = [
+    {
+        "quote": "This world is rotten, and those who are making it rot deserve to die.",
+        "character": "Light Yagami",
+        "anime": "Death Note"
+    },
+    {
+        "quote": "The world is cruel, but it’s also very beautiful.",
+        "character": "Mikasa Ackerman",
+        "anime": "Attack on Titan"
+    },
+    {
+        "quote": "If you don’t fight, you can’t win.",
+        "character": "Eren Yeager",
+        "anime": "Attack on Titan"
+    },
+    {
+        "quote": "Everyone had to be drunk on something to keep pushing on.",
+        "character": "Kenny Ackerman",
+        "anime": "Attack on Titan"
+    },
+    {
+        "quote": "If you win, you live. If you lose, you die. If you don’t fight, you can’t win.",
+        "character": "Levi Ackerman",
+        "anime": "Attack on Titan"
+    },
+    {
+        "quote": "People who can’t throw something important away can never hope to change anything.",
+        "character": "Armin Arlert",
+        "anime": "Attack on Titan"
+    },
+    {
+        "quote": "Genius is not about making the right move, it’s about making the opponent believe you did.",
+        "character": "Tokuchi Toua",
+        "anime": "One Outs"
+    },
+    {
+        "quote": "Time doesn’t heal wounds. It just teaches us how to live with them.",
+        "character": "Lu Guang",
+        "anime": "Link Click"
+    },
+    {
+        "quote": "There’s no shortcut to success. You just keep driving forward.",
+        "character": "Takumi Fujiwara",
+        "anime": "Initial D"
+    },
+    {
+        "quote": "A true warrior doesn’t need a sword.",
+        "character": "Thors",
+        "anime": "Vinland Saga"
+    }
+]
+
+export default quotes;
